a1ac2b295cd4b19f15e0802f16f644bca6bfddfc
Rafinhaa <rafinhaa@me.com>
2024-09-11 18:16:05 -0300

    feat: summary

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,14 @@
 import { Dialog } from "./components/ui/dialog"
 import { CreateGoal } from "./components/create-goal"
 import { EmptyGoal } from "./components/empty-goal"
+import { Summary } from "./components/summary"
 
 export const App = () => {
   return (
     <Dialog>
-      <EmptyGoal />
+      {/* <EmptyGoal /> */}
 
+      <Summary />
       <CreateGoal />
     </Dialog>
   )
