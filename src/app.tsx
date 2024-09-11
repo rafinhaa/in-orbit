e5a9e8a31814256495da36e9a8a338c794958abb
Rafinhaa <rafinhaa@me.com>
2024-09-11 16:58:20 -0300

    feat: first page

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,24 @@
+import logo from "./assets/logo-in-orbit.svg"
+import letsStart from "./assets/lets-start-illustration.svg"
+import { Plus } from "lucide-react"
+
 export const App = () => {
-  return <h1 className="bg-red-500 text-5xl font-bold underline">Olá mundo!</h1>
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in-orbit-logo" />
+      <img src={letsStart} alt="lets-start" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
+        mesmo?
+      </p>
+
+      <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-3 text-sm font-medium tracking-tight hover:bg-violet-600"
+      >
+        <Plus className="size-4" />
+        Cadastrar meta
+      </button>
+    </div>
+  )
 }
